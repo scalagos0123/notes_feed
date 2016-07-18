51ea9ac4a64518c24a2c22d4bbcf441857fab881
Shaun Calagos <scalagos0123@live.com>
2016-07-18 10:22:02 +0800

    recompiled misc.xml

--- app/src/main/java/mobidev/com/notesfeed/LoginActivity.java
+++ app/src/main/java/mobidev/com/notesfeed/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -189,9 +190,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
         }
-
-        Intent gotoMain = new Intent(this, NotesFeed_main.class);
-        startActivity(gotoMain);
     }
 
     private boolean isEmailValid(String email) {
