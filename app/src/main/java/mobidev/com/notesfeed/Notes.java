db87d470ef17899ffab7f6a0c6572211e93b7d4c
Shaun Calagos <scalagos0123@live.com>
2016-08-07 23:04:27 +0800

    * added getNoteOwner

--- app/src/main/java/mobidev/com/notesfeed/Notes.java
+++ app/src/main/java/mobidev/com/notesfeed/Notes.java
@@ -10,12 +10,33 @@ public class Notes implements Serializable {
     private int notes_id;
     private String notes_title;
     private String notes_content;
+    private String note_owner;
 
 
     public Notes(int notes_id, String notes_title, String notes_content) {
         this.notes_id = notes_id;
         this.notes_title = notes_title;
         this.notes_content = notes_content;
+    }
+
+    public void setNotes_id(int notes_id) {
+        this.notes_id = notes_id;
+    }
+
+    public void setNotes_title(String notes_title) {
+        this.notes_title = notes_title;
+    }
+
+    public void setNotes_content(String notes_content) {
+        this.notes_content = notes_content;
+    }
+
+    public String getNote_owner() {
+        return note_owner;
+    }
+
+    public void setNote_owner(String note_owner) {
+        this.note_owner = note_owner;
     }
 
     public int getNotes_id() { return notes_id; }
