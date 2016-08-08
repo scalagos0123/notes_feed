e299a23ea2fcae43faea29a6bd1df604ef0067bd
Shaun Calagos <scalagos0123@live.com>
2016-08-08 19:47:58 +0800

    * added methods to get current user's information, such as email, password, and his/her name

--- app/src/main/java/mobidev/com/notesfeed/NotesFeedSession.java
+++ app/src/main/java/mobidev/com/notesfeed/NotesFeedSession.java
@@ -13,6 +13,8 @@ public class NotesFeedSession {
     public final String SHARED_PREFERENCES = "Session";
     public final String SESSION_USER_ID = "userId";
     public final String SESSION_USER_NAME = "user_fullname";
+    public final String SESSION_USER_PASSWORD = "user_password";
+    public final String SESSION_USER_EMAIL = "user_email";
     public final static String SERVER_ADDRESS = "http://192.168.254.109/";
     public final static String SERVER_ADDRESS_ALTERNATE = "http://SHAUN-G501/";
 
@@ -26,22 +28,57 @@ public class NotesFeedSession {
         editor.commit();
     }
 
-    public void startUserSession(String userId, String fullname) {
+    public void startUserSession(String userId, String fullname, String password, String email) {
         editor.putString(SESSION_USER_ID, userId);
         editor.putString(SESSION_USER_NAME, fullname);
+        editor.putString(SESSION_USER_EMAIL, email);
+        editor.putString(SESSION_USER_PASSWORD, password);
         editor.commit();
     }
 
-    public void editUserSession(String userId, String fullname) {
+    public void editUserSessionName(String fullname) {
 
         if (!context.getSharedPreferences(SHARED_PREFERENCES, context.MODE_PRIVATE).getString(SESSION_USER_ID, null).equals(null) || !context.getSharedPreferences(SHARED_PREFERENCES, context.MODE_PRIVATE).getString(SESSION_USER_NAME, null).equals(null)) {
 
-            editor.putString(SESSION_USER_ID, userId);
             editor.putString(SESSION_USER_NAME, fullname);
             editor.commit();
 
         } else {
             endUserSession();
         }
+    }
+
+    public void editUserSessionPassword(String password) {
+        if (!context.getSharedPreferences(SHARED_PREFERENCES, context.MODE_PRIVATE).getString(SESSION_USER_ID, null).equals(null) || !context.getSharedPreferences(SHARED_PREFERENCES, context.MODE_PRIVATE).getString(SESSION_USER_PASSWORD, null).equals(null)) {
+
+            editor.putString(SESSION_USER_PASSWORD, password);
+            editor.commit();
+
+        } else {
+            endUserSession();
+        }
+    }
+
+    public void editUserSessionEmail(String email) {
+        if (!context.getSharedPreferences(SHARED_PREFERENCES, context.MODE_PRIVATE).getString(SESSION_USER_ID, null).equals(null) || !context.getSharedPreferences(SHARED_PREFERENCES, context.MODE_PRIVATE).getString(SESSION_USER_EMAIL, null).equals(null)) {
+
+            editor.putString(SESSION_USER_EMAIL, email);
+            editor.commit();
+
+        } else {
+            endUserSession();
+        }
+    }
+
+    public String getUserFullName() {
+        return context.getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE).getString(SESSION_USER_NAME, null);
+    }
+
+    public String getUserEmail() {
+        return context.getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE).getString(SESSION_USER_EMAIL, null);
+    }
+
+    public String getUserPassword() {
+        return context.getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE).getString(SESSION_USER_PASSWORD, null);
     }
 }
