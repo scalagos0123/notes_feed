977467dcb3339b2c2006d15480212e88c7a14d5e
Shaun Calagos <scalagos0123@live.com>
2016-08-08 21:02:47 +0800

    * added item select listeners

--- app/src/main/java/mobidev/com/notesfeed/Tab3.java
+++ app/src/main/java/mobidev/com/notesfeed/Tab3.java
@@ -12,6 +12,7 @@ import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,22 +22,47 @@ import android.widget.ListView;
  */
 
 //Our class extending fragment
-public class  Tab3 extends Fragment {
+public class Tab3 extends Fragment {
 
-
+    ListView list = null;
+    String[] myItems = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_fragment_3, container, false);
         populateListView(view);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemSelected = (String) parent.getItemAtPosition(position);
+
+                if (itemSelected.equals(myItems[0])) {
+
+//                    start the intent here kung change password yung sinelect
+
+                } else if (itemSelected.equals(myItems[1])) {
+
+//                    start the intent here kung change email yung sinelect
+
+                } else if (itemSelected.equals(myItems[2])) {
+//                    Don't mess this part
+                    NotesFeedSession n = new NotesFeedSession(getContext());
+                    n.endUserSession();
+                    getActivity().finish();
+                }
+            }
+        });
+
         return view;
     }
 
     private void populateListView(View v){
         String[] myItems = {"Change Password", "Change Email", "Sign Out"};
+        this.myItems = myItems;
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, myItems);
 
-        ListView list = (ListView) v.findViewById(R.id.listViewMain);
+        list = (ListView) v.findViewById(R.id.listViewMain);
         list.setAdapter(adapter);
     }
 }
