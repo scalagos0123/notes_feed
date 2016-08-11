47f3e9e8ca4d8539905f5676db1ca2426ea27cd7
zyxcbaco <bendrhick_co@yahoo.com>
2016-08-11 23:38:35 +0800

    g

---
+++ app/src/main/java/mobidev/com/notesfeed/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package mobidev.com.notesfeed;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Debbie Co on 8/6/2016.
+ */
+public class DatabaseHelper  extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Notes.db";
+
+    public static final String TABLE_NAME = "my_notes";
+    public static final String COL_1 = "notes_id";
+    public static final String COL_2 = "notes_title";
+    public static final String COL_3 = "notes_content";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    db.execSQL("create table" + TABLE_NAME + "(NOTES_ID INTEGER PRIMARY KEY AUTOINCREMENT,  NOTES_TITLE TEXT, NOTES_CONTENT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+        public boolean insertData(String my_notes, String notes_id, String notes_title,String notes_content){
+             SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(COL_1, notes_id);
+            contentValues.put(COL_2, notes_title);
+            contentValues.put(COL_3, notes_content);
+            long result = db.insert(my_notes,null,contentValues);
+            if(result== -1)
+                return false;
+            else
+                return true;
+    }
+}
