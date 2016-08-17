144ae1d1274a8742ecd30b62ba214e5586e81d16
Shaun Calagos <scalagos0123@live.com>
2016-08-18 01:30:29 +0800

    * added findgroup activity

---
+++ app/src/main/java/mobidev/com/notesfeed/FindGroup.java
@@ -0,0 +1,13 @@
+package mobidev.com.notesfeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_group);
+    }
+}
