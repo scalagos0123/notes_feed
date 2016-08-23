e9a6e4eb57d87ae8868480267521d74e23cb2402
Shaun Calagos <scalagos0123@live.com>
2016-08-24 00:27:50 +0800

    * change email works

--- app/src/main/java/mobidev/com/notesfeed/ChangeEmail.java
+++ app/src/main/java/mobidev/com/notesfeed/ChangeEmail.java
@@ -7,21 +7,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
  * Created by Kat on 11/08/2016.
  */
-public class ChangeEmail extends AppCompatActivity{
+public class ChangeEmail extends AppCompatActivity {
     private NotesFeedSession n;
+    private String newEmail1;
+    private ChangeEmail c = this;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_email);
 
         Button btnChangeEmail = (Button)findViewById(R.id.button1);
+    }
+
+    public void done() {
+        finish();
     }
 
     public void changeEmail(View view) {
@@ -31,25 +41,21 @@ public class ChangeEmail extends AppCompatActivity{
         n = new NotesFeedSession(this);
 
         String currentEmail1 = n.getUserEmail();
-        String newEmail1 = newEmail.getText().toString();
+        newEmail1 = newEmail.getText().toString();
         String retypeEmail1 = retypeEmail.getText().toString();
 
 
         if (currentEmail.getText().toString().equals(currentEmail1) && currentEmail.getText().toString().contains("@")) {
-            if (newEmail.getText().toString().equals(retypeEmail1)) {
+            if (newEmail1.equals(retypeEmail1)) {
                 //set
 
 
                 //check the edittext input if it has @
                 //
 
-                n.editUserSessionEmail(newEmail1);
-                System.out.println(n.getUserEmail());
-
                 Change_Email n = new Change_Email();
                 n.execute(newEmail1);
-
-                finish();
+                done();
             } else {
                 currentEmail.setTextColor(getResources().getColor(R.color.redLine));
                 retypeEmail.setTextColor(getResources().getColor(R.color.redLine));
@@ -64,6 +70,7 @@ public class ChangeEmail extends AppCompatActivity{
         @Override
         protected Boolean doInBackground(String... params) {
             String m = params[0];
+            System.out.println(m);
             String link = NotesFeedSession.SERVER_ADDRESS + "/notesfeed/changeemail.php";
             String body = "user_id=" + n.getUserId() + "&user_email=" + m;
             byte[] bodyBytes = body.getBytes();
@@ -73,7 +80,23 @@ public class ChangeEmail extends AppCompatActivity{
                 URL url = new URL(link);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
                 urlConnection.getOutputStream().write(bodyBytes);
+
+                BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+
+                for (int i; (i = r.read()) >= 0;) {
+                    sb.append((char) i);
+                }
+
+                System.out.println(sb);
+                if (sb.toString().equals("true")) {
+                    status = true;
+                } else {
+                    status = false;
+                    System.out.println(sb.toString());
+                }
 
 
             } catch (Exception e) {
@@ -86,6 +109,13 @@ public class ChangeEmail extends AppCompatActivity{
         @Override
         protected void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
+
+            if (aBoolean) {
+                n.editUserSessionEmail(newEmail1);
+                System.out.println(n.getUserEmail());
+
+                Toast.makeText(c, "Email changed", Toast.LENGTH_SHORT);
+            }
         }
     }
 
